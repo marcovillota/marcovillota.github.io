938906d129ebe55f11382bc0b8238176aecef9cc
marcovillota <marcvillot00@gmail.com>
2024-12-12 16:24:57 -0500

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+import parallax from './components/parallax.js';
+import activeMenu from './components/selected_menu.js';
+import updateCopyRightYear from './helpers/date_updater.js';
+import loader from './helpers/loader.js';
+import resetToHome from './helpers/reload_page.js';
+import sendEmail from './helpers/send_form.js';
+
+loader();
+resetToHome();
+parallax();
+activeMenu();
+updateCopyRightYear();
+sendEmail();
