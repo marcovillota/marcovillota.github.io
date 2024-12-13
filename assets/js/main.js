e84c23409c5906ca9c1836e579418d47a2a4347e
marcovillota <marcvillot00@gmail.com>
2024-12-13 12:37:02 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 import parallax from './components/parallax.js';
 import activeMenu from './components/selected_menu.js';
 import updateCopyRightYear from './helpers/date_updater.js';
+import changeLang from './helpers/language.js';
 import loader from './helpers/loader.js';
 import resetToHome from './helpers/reload_page.js';
 import sendEmail from './helpers/send_form.js';
